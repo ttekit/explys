cab028f2aa2b158a33cac93f606dc2e081ecdeef
hgti32 <127024694+hgti32@users.noreply.github.com>
2026-03-25 16:06:10 +0200

    CRUD update

--- backend/src/contents/contents.controller.ts
+++ backend/src/contents/contents.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ContentsService } from './contents.service';
 
 @Controller('contents')
 export class ContentsController {
   constructor(private readonly contentsService: ContentsService) {}
+
+  @Get()
+  getContent() {
+    return this.contentsService.getAllContent();
+  }
+
+
 }
+
+
