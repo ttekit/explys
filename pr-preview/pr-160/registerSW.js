c59f8fbe0c7ec1a35a614a358c84888907e32b45
ivan-koltsov <ivan-koltsov@users.noreply.github.com>
2026-09-12 12:09:13 +0000

    Deploy preview for PR 160 🛫

--- pr-preview/pr-160/registerSW.js
+++ pr-preview/pr-160/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/explys/pr-preview/pr-160/sw.js', { scope: '/explys/pr-preview/pr-160/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pr-preview/pr-160/sw.js', { scope: '/pr-preview/pr-160/' })})}
