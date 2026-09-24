26ea91167e856e833524f5e54d6b863f3c24c9e3
leormix <leormix@users.noreply.github.com>
2026-09-24 13:27:29 +0000

    Remove preview for PR 165 🛬

--- pr-preview/pr-165/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/explys/pr-preview/pr-165/sw.js', { scope: '/explys/pr-preview/pr-165/' })})}
