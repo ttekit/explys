205c5c8905869377434122a5085cbf3b4b2a39a7
ttekit <ttekit@users.noreply.github.com>
2026-09-24 16:18:29 +0000

    Remove preview for PR 162 🛬

--- pr-preview/pr-162/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/explys/pr-preview/pr-162/sw.js', { scope: '/explys/pr-preview/pr-162/' })})}
